e22e40ade68e34adda54b03122ec907b7b7a9540
Michael Babker <mbabker@flbab.com>
2013-09-29 16:43:18 -0500

    Manifest cleanup, uninstall SQL

---
+++ administrator/components/com_code/install/mysql.uninstall.sql
@@ -0,0 +1,33 @@
+DROP TABLE IF EXISTS `#__code_activity_detail`;
+
+DROP TABLE IF EXISTS `#__code_activity_types`;
+
+DROP TABLE IF EXISTS `#__code_branches`;
+
+DROP TABLE IF EXISTS `#__code_builds`;
+
+DROP TABLE IF EXISTS `#__code_nightly_builds`;
+
+DROP TABLE IF EXISTS `#__code_projects`;
+
+DROP TABLE IF EXISTS `#__code_tags`;
+
+DROP TABLE IF EXISTS `#__code_trackers`;
+
+DROP TABLE IF EXISTS `#__code_tracker_issues`;
+
+DROP TABLE IF EXISTS `#__code_tracker_issue_assignments`;
+
+DROP TABLE IF EXISTS `#__code_tracker_issue_changes`;
+
+DROP TABLE IF EXISTS `#__code_tracker_issue_commits`;
+
+DROP TABLE IF EXISTS `#__code_tracker_issue_files`;
+
+DROP TABLE IF EXISTS `#__code_tracker_issue_responses`;
+
+DROP TABLE IF EXISTS `#__code_tracker_snapshots`;
+
+DROP TABLE IF EXISTS `#__code_tracker_issue_tag_map`;
+
+DROP TABLE IF EXISTS `#__code_users`;
