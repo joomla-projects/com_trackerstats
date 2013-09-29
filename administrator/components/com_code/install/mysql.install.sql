4310e63ac390081324c5ecfc5333b87ab3cdd058
Michael Babker <mbabker@flbab.com>
2013-09-29 16:15:31 -0500

    Reorder table install to match phpMyAdmin display, use #_ prefix

--- administrator/components/com_code/install/mysql.install.sql
+++ administrator/components/com_code/install/mysql.install.sql
@@ -1,23 +1,4 @@
-CREATE TABLE IF NOT EXISTS `jos_code_activity_types` (
-  `activity_type` tinyint(4) default NULL COMMENT '1-create; 2-comment; 3-change; 4-test; 5-patch; 6-pull in comment; 7-pull in description',
-  `activity_title` varchar(255) default NULL COMMENT 'Title for each type',
-  `activity_group` varchar(255) default NULL COMMENT 'Tracker, Test, Code',
-  `activity_description` varchar(500) default NULL COMMENT 'Activity description',
-  `activity_points` tinyint(4) default NULL COMMENT 'Weighting for each type of activity',
-  PRIMARY KEY  (`activity_type`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-INSERT INTO `jos_code_activity_types` 
-(`activity_type`, `activity_title`, `activity_group`, `activity_description`, `activity_points`) VALUES
-(1, 'Create Issue', 'Tracker', 'Create a new issue in tracker.', 3),
-(2, 'Comment Issue', 'Tracker', 'Add a comment to an issue.', 1),
-(3, 'Change Issue', 'Tracker', 'Change the status of an issue.', 1),
-(4, 'Test Issue', 'Test', 'Test an issue.', 5),
-(5, 'Patch Issue', 'Code', 'Create a patch or diff file for an issue.', 5),
-(6, 'Pull Request in Comment', 'Code', 'Add a pull request link in a comment.', 5),
-(7, 'Pull Request in Description', 'Code', 'Add a pull request link in the original issue description.', 5);
-
-CREATE TABLE IF NOT EXISTS `jos_code_activity_detail` (
+CREATE TABLE IF NOT EXISTS `#__code_activity_detail` (
   `activity_type` tinyint(4) NOT NULL DEFAULT '0' COMMENT '1-create; 2-comment; 3-change; 4-test; 5-patch; 6-pull in comment; 7-pull in description',
   `activity_xref_id` int(10) unsigned NOT NULL COMMENT 'id for issue, response, change, or file',
   `jc_user_id` int(11) DEFAULT NULL,
@@ -29,8 +10,26 @@ CREATE TABLE IF NOT EXISTS `jos_code_activity_detail` (
   KEY `idx_jc_issue_id` (`jc_issue_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE IF NOT EXISTS `#__code_activity_types` (
+  `activity_type` tinyint(4) default NULL COMMENT '1-create; 2-comment; 3-change; 4-test; 5-patch; 6-pull in comment; 7-pull in description',
+  `activity_title` varchar(255) default NULL COMMENT 'Title for each type',
+  `activity_group` varchar(255) default NULL COMMENT 'Tracker, Test, Code',
+  `activity_description` varchar(500) default NULL COMMENT 'Activity description',
+  `activity_points` tinyint(4) default NULL COMMENT 'Weighting for each type of activity',
+  PRIMARY KEY  (`activity_type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `jos_code_branches` (
+INSERT INTO `#__code_activity_types`
+(`activity_type`, `activity_title`, `activity_group`, `activity_description`, `activity_points`) VALUES
+(1, 'Create Issue', 'Tracker', 'Create a new issue in tracker.', 3),
+(2, 'Comment Issue', 'Tracker', 'Add a comment to an issue.', 1),
+(3, 'Change Issue', 'Tracker', 'Change the status of an issue.', 1),
+(4, 'Test Issue', 'Test', 'Test an issue.', 5),
+(5, 'Patch Issue', 'Code', 'Create a patch or diff file for an issue.', 5),
+(6, 'Pull Request in Comment', 'Code', 'Add a pull request link in a comment.', 5),
+(7, 'Pull Request in Description', 'Code', 'Add a pull request link in the original issue description.', 5);
+
+CREATE TABLE IF NOT EXISTS `#__code_branches` (
   `branch_id` int(10) unsigned NOT NULL auto_increment,
   `project_id` int(10) unsigned NOT NULL,
   `asset_id` int(11) NOT NULL,
@@ -47,7 +46,7 @@ CREATE TABLE `jos_code_branches` (
   PRIMARY KEY  (`branch_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-CREATE TABLE `jos_code_builds` (
+CREATE TABLE IF NOT EXISTS `#__code_builds` (
   `build_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `revision_id` int(10) unsigned DEFAULT NULL,
   `branch_id` int(10) unsigned DEFAULT NULL,
@@ -85,7 +84,7 @@ CREATE TABLE `jos_code_builds` (
   KEY `idx_branch_revision` (`branch_id`,`revision_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-CREATE TABLE `jos_code_nightly_builds` (
+CREATE TABLE IF NOT EXISTS `#__code_nightly_builds` (
   `build_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `project_id` int(10) unsigned DEFAULT NULL,
   `branch_id` int(10) unsigned DEFAULT NULL,
@@ -120,7 +119,7 @@ CREATE TABLE `jos_code_nightly_builds` (
   KEY `idx_branch_revision` (`branch_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-CREATE TABLE `jos_code_projects` (
+CREATE TABLE IF NOT EXISTS `#__code_projects` (
   `project_id` int(10) unsigned NOT NULL auto_increment,
   `asset_id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
@@ -137,7 +136,7 @@ CREATE TABLE `jos_code_projects` (
   PRIMARY KEY  (`project_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `jos_code_tags` (
+CREATE TABLE IF NOT EXISTS `#__code_tags` (
   `tag_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `tag` varchar(512) DEFAULT NULL,
   `total` int(10) unsigned NOT NULL,
@@ -145,86 +144,30 @@ CREATE TABLE `jos_code_tags` (
   KEY `idx_code_tags_total` (`total`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `jos_code_tracker_issue_assignments` (
-  `issue_id` int(10) unsigned NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `jc_user_id` int(11) DEFAULT NULL,
-  `jc_issue_id` int(11) DEFAULT NULL,
-  KEY `issue_id` (`issue_id`,`user_id`),
-  KEY `jc_issue_id` (`jc_issue_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `jos_code_tracker_issue_changes` (
-  `change_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `issue_id` int(10) unsigned NOT NULL,
-  `tracker_id` int(10) unsigned NOT NULL,
-  `change_date` datetime NOT NULL,
-  `change_by` int(11) NOT NULL,
-  `data` text NOT NULL,
-  `jc_change_id` int(11) DEFAULT NULL,
-  `jc_issue_id` int(11) DEFAULT NULL,
-  `jc_tracker_id` int(11) DEFAULT NULL,
-  `jc_change_by` int(11) DEFAULT NULL,
-  PRIMARY KEY (`change_id`),
-  UNIQUE KEY `jc_change_id` (`jc_change_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `jos_code_tracker_issue_commits` (
-  `commit_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `issue_id` int(10) unsigned NOT NULL,
-  `tracker_id` int(10) unsigned NOT NULL,
+CREATE TABLE IF NOT EXISTS `#__code_trackers` (
+  `tracker_id` int(10) unsigned NOT NULL auto_increment,
+  `project_id` int(10) unsigned NOT NULL,
+  `asset_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `alias` varchar(255) NOT NULL,
+  `summary` varchar(512) NOT NULL,
+  `description` text NOT NULL,
+  `state` int(11) NOT NULL,
+  `access` int(11) NOT NULL,
+  `options` text NOT NULL,
+  `metadata` text NOT NULL,
+  `item_count` int(11) NOT NULL,
+  `open_item_count` int(11) NOT NULL,
   `created_date` datetime NOT NULL,
   `created_by` int(11) NOT NULL,
-  `message` text NOT NULL,
-  `jc_commit_id` int(11) DEFAULT NULL,
-  `jc_issue_id` int(11) DEFAULT NULL,
-  `jc_tracker_id` int(11) DEFAULT NULL,
-  `jc_created_by` int(11) DEFAULT NULL,
-  PRIMARY KEY (`commit_id`),
-  UNIQUE KEY `jc_commit_id` (`jc_commit_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `modified_date` datetime NOT NULL,
+  `modified_by` int(11) NOT NULL,
+  `jc_tracker_id` int(11) default NULL,
+  `jc_project_id` int(11) default NULL,
+  PRIMARY KEY  (`tracker_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-CREATE TABLE `jos_code_tracker_issue_files` (
-  `file_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `issue_id` int(10) unsigned NOT NULL,
-  `tracker_id` int(10) unsigned NOT NULL,
-  `created_date` datetime NOT NULL,
-  `created_by` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `description` varchar(512) NOT NULL,
-  `size` int(11) NOT NULL,
-  `type` varchar(255) NOT NULL,
-  `jc_file_id` int(11) DEFAULT NULL,
-  `jc_issue_id` int(11) DEFAULT NULL,
-  `jc_tracker_id` int(11) DEFAULT NULL,
-  `jc_created_by` int(11) DEFAULT NULL,
-  PRIMARY KEY (`file_id`),
-  UNIQUE KEY `idx_issue_files_legacy` (`jc_file_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `jos_code_tracker_issue_responses` (
-  `response_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `issue_id` int(10) unsigned NOT NULL,
-  `tracker_id` int(10) unsigned NOT NULL,
-  `created_date` datetime NOT NULL,
-  `created_by` int(11) NOT NULL,
-  `body` text NOT NULL,
-  `jc_response_id` int(11) DEFAULT NULL,
-  `jc_issue_id` int(11) DEFAULT NULL,
-  `jc_tracker_id` int(11) DEFAULT NULL,
-  `jc_created_by` int(11) DEFAULT NULL,
-  PRIMARY KEY (`response_id`),
-  UNIQUE KEY `idx_tracker_responses_legacy` (`jc_response_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `jos_code_tracker_issue_tag_map` (
-  `issue_id` int(10) unsigned DEFAULT NULL,
-  `tag_id` int(10) unsigned DEFAULT NULL,
-  `tag` varchar(255) DEFAULT NULL,
-  KEY `issue_id` (`issue_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `jos_code_tracker_issues` (
+CREATE TABLE IF NOT EXISTS `#__code_tracker_issues` (
   `issue_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `tracker_id` int(10) unsigned NOT NULL,
   `project_id` int(10) unsigned NOT NULL,
@@ -250,30 +193,94 @@ CREATE TABLE `jos_code_tracker_issues` (
   UNIQUE KEY `idx_tracker_issues_legacy` (`jc_issue_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `jos_code_trackers` (
-  `tracker_id` int(10) unsigned NOT NULL auto_increment,
-  `project_id` int(10) unsigned NOT NULL,
-  `asset_id` int(11) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `alias` varchar(255) NOT NULL,
-  `summary` varchar(512) NOT NULL,
-  `description` text NOT NULL,
-  `state` int(11) NOT NULL,
-  `access` int(11) NOT NULL,
-  `options` text NOT NULL,
-  `metadata` text NOT NULL,
-  `item_count` int(11) NOT NULL,
-  `open_item_count` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `#__code_tracker_issue_assignments` (
+  `issue_id` int(10) unsigned NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `jc_user_id` int(11) DEFAULT NULL,
+  `jc_issue_id` int(11) DEFAULT NULL,
+  KEY `issue_id` (`issue_id`,`user_id`),
+  KEY `jc_issue_id` (`jc_issue_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `#__code_tracker_issue_changes` (
+  `change_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `issue_id` int(10) unsigned NOT NULL,
+  `tracker_id` int(10) unsigned NOT NULL,
+  `change_date` datetime NOT NULL,
+  `change_by` int(11) NOT NULL,
+  `data` text NOT NULL,
+  `jc_change_id` int(11) DEFAULT NULL,
+  `jc_issue_id` int(11) DEFAULT NULL,
+  `jc_tracker_id` int(11) DEFAULT NULL,
+  `jc_change_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`change_id`),
+  UNIQUE KEY `jc_change_id` (`jc_change_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `#__code_tracker_issue_commits` (
+  `commit_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `issue_id` int(10) unsigned NOT NULL,
+  `tracker_id` int(10) unsigned NOT NULL,
   `created_date` datetime NOT NULL,
   `created_by` int(11) NOT NULL,
-  `modified_date` datetime NOT NULL,
-  `modified_by` int(11) NOT NULL,
-  `jc_tracker_id` int(11) default NULL,
-  `jc_project_id` int(11) default NULL,
-  PRIMARY KEY  (`tracker_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `message` text NOT NULL,
+  `jc_commit_id` int(11) DEFAULT NULL,
+  `jc_issue_id` int(11) DEFAULT NULL,
+  `jc_tracker_id` int(11) DEFAULT NULL,
+  `jc_created_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`commit_id`),
+  UNIQUE KEY `jc_commit_id` (`jc_commit_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `jos_code_users` (
+CREATE TABLE IF NOT EXISTS `#__code_tracker_issue_files` (
+  `file_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `issue_id` int(10) unsigned NOT NULL,
+  `tracker_id` int(10) unsigned NOT NULL,
+  `created_date` datetime NOT NULL,
+  `created_by` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(512) NOT NULL,
+  `size` int(11) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `jc_file_id` int(11) DEFAULT NULL,
+  `jc_issue_id` int(11) DEFAULT NULL,
+  `jc_tracker_id` int(11) DEFAULT NULL,
+  `jc_created_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`file_id`),
+  UNIQUE KEY `idx_issue_files_legacy` (`jc_file_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `#__code_tracker_issue_responses` (
+  `response_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `issue_id` int(10) unsigned NOT NULL,
+  `tracker_id` int(10) unsigned NOT NULL,
+  `created_date` datetime NOT NULL,
+  `created_by` int(11) NOT NULL,
+  `body` text NOT NULL,
+  `jc_response_id` int(11) DEFAULT NULL,
+  `jc_issue_id` int(11) DEFAULT NULL,
+  `jc_tracker_id` int(11) DEFAULT NULL,
+  `jc_created_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`response_id`),
+  UNIQUE KEY `idx_tracker_responses_legacy` (`jc_response_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `#__code_tracker_snapshots` (
+  `tracker_id` int(10) unsigned NOT NULL,
+  `snapshot_day` varchar(10) NOT NULL,
+  `modified_date` datetime NOT NULL,
+  `status_counts` varchar(512) NOT NULL,
+  PRIMARY KEY (`tracker_id`,`snapshot_day`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `#__code_tracker_issue_tag_map` (
+  `issue_id` int(10) unsigned DEFAULT NULL,
+  `tag_id` int(10) unsigned DEFAULT NULL,
+  `tag` varchar(255) DEFAULT NULL,
+  KEY `issue_id` (`issue_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `#__code_users` (
   `user_id` int(11) NOT NULL auto_increment,
   `first_name` varchar(255) NOT NULL,
   `last_name` varchar(255) NOT NULL,
@@ -290,12 +297,4 @@ CREATE TABLE `jos_code_users` (
   `jca_document_id` varchar(255) NOT NULL,
   `signed_jca` int(1) unsigned NOT NULL,
   `jc_user_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE IF NOT EXISTS `eem_code_tracker_snapshots` (
-  `tracker_id` int(10) unsigned NOT NULL,
-  `snapshot_day` varchar(10) NOT NULL,
-  `modified_date` datetime NOT NULL,
-  `status_counts` varchar(512) NOT NULL,
-  PRIMARY KEY (`tracker_id`,`snapshot_day`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
