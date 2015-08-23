9726d902ed467fd76aea3fecc709cb4f25d52541
Michael Babker <mbabker@flbab.com>
2015-08-22 20:34:13 -0400

    Move to the media folder

--- components/com_trackerstats/helpers/html/barchart.php
+++ components/com_trackerstats/helpers/html/barchart.php
@@ -41,129 +41,86 @@ abstract class JHtmlBarchart
 		$orientation = ($horizontal == true) ? 'horizontal' : 'vertical';
 
 		// Depends on jQuery UI
-		$document = JFactory::getDocument();
 		JHtml::_('bootstrap.framework');
-		$document->addScript('components/com_trackerstats/media/js/jquery.jqplot.min.js', 'text/javascript', false);
-		$document->addScript('components/com_trackerstats/media/js/jqplot.barRenderer.min.js', 'text/javascript', true);
-		$document->addScript('components/com_trackerstats/media/js/jqplot.categoryAxisRenderer.min.js', 'text/javascript', true);
-		$document->addScript('components/com_trackerstats/media/js/jqplot.pointLabels.min.js', 'text/javascript', true);
-		$document->addScript('components/com_trackerstats/media/js/barchart.js', 'text/javascript', true);
-		$document->addScript('components/com_trackerstats/media/js/jquery-ui-1.10.2.custom.min.js', 'text/javascript', true);
-		$document->addScript('components/com_trackerstats/media/js/jqplot.highlighter.min.js', 'text/javascript', true);
-		$document->addStyleSheet(JUri::root(true) . '/components/com_trackerstats/media/css/jquery.jqplot.min.css');
-		$document->addStyleSheet(JUri::root(true) . '/components/com_trackerstats/media/css/jquery-ui-1.10.2.custom.min.css');
+		JHtml::_('script', 'com_trackerstats/jquery.jqplot.min.js', false, true);
+		JHtml::_('script', 'com_trackerstats/jqplot.barRenderer.min.js', false, true);
+		JHtml::_('script', 'com_trackerstats/jqplot.categoryAxisRenderer.min.js', false, true);
+		JHtml::_('script', 'com_trackerstats/jqplot.pointLabels.min.js', false, true);
+		JHtml::_('script', 'com_trackerstats/barchart.js', false, true);
+		JHtml::_('script', 'com_trackerstats/jquery-ui-1.10.2.custom.min.js', false, true);
+		JHtml::_('script', 'com_trackerstats/jqplot.highlighter.min.js', false, true);
+		JHtml::_('stylesheet', 'com_trackerstats/jquery.jqplot.min.css', [], true);
+		JHtml::_('stylesheet', 'com_trackerstats/jquery-ui-1.10.2.custom.min.css', [], true);
+
+		$document = JFactory::getDocument();
 
 		// Attach sortable to document
-		$document->addScriptDeclaration("
-			(function ($){
-				$(document).ready(function (){
-					var barchart = new $.JQPLOTBarchart('" . $containerId . "','" . $urlId . "','" . $orientation . "','" . $stackSeries . "','" . $barMargin . "');
-					});
-			})(jQuery);
-			"
+		$document->addScriptDeclaration(<<<JS
+
+		(function ($) {
+			$(document).ready(function () {
+				var barchart = new $.JQPLOTBarchart('$containerId','$urlId','$orientation','$stackSeries','$barMargin');
+			});
+		})(jQuery);
+
+JS
 		);
-		$document->addScriptDeclaration("
-			(function ($){
-				$(document).ready(function (){
-    			$('#dataUpdate').click(function() {
-					$('#" . $containerId . "').empty();
+
+		$document->addScriptDeclaration(<<<JS
+
+		(function ($) {
+			$(document).ready(function () {
+				$('#dataUpdate').click(function () {
+					$('#$containerId').empty();
+
 					// add the form variables to the URL
 					var period = $('#period').val();
 					var type = $('#type').val();
-					var href = $('#" . $urlId . "').attr('data-href');
+					var href = $('#$urlId').attr('data-href');
 					var startdate = $('#start_date').val();
 					var enddate = $('#end_date').val();
 					href = href.substr(0, href.indexOf('format=json') + 11);
 					href = href + '&amp;period=' + period + '&amp;activity_type=' + type;
-					if (period == 5)
-					{
+
+					if (period == 5) {
 						href = href + '&amp;startdate=' + startdate + '&amp;enddate=' + enddate;
 					}
-					$('#" . $urlId . "').attr('data-href', href);
-					var barChart = new $.JQPLOTBarchart('" . $containerId . "','" . $urlId . "','" . $orientation . "','" . $stackSeries . "','" . $barMargin . "');
+
+					$('#$urlId').attr('data-href', href);
+					var barChart = new $.JQPLOTBarchart('$containerId','$urlId','$orientation','$stackSeries','$barMargin');
 				});
-				});
-			})(jQuery);
-			"
+			});
+		})(jQuery);
+
+JS
 		);
-		$document->addScriptDeclaration("
+		$document->addScriptDeclaration(<<<JS
+
 		/*
 		 * jQuery UI Datepicker: Parse and Format Dates
 		 * http://salman-w.blogspot.com/2013/01/jquery-ui-datepicker-examples.html
 		 */
 		(function($) {
-			$(document).ready(function (){
-			$('.datepicker').datepicker({
-				dateFormat: 'yy-mm-dd',
-				onSelect: function(dateText, inst) {
-					var date = $.datepicker.parseDate(inst.settings.dateFormat || $.datepicker._defaults.dateFormat, dateText, inst.settings);
-					var dateText1 = $.datepicker.formatDate('D, d M yy', date, inst.settings);
-					date.setDate(date.getDate() + 7);
-					var dateText2 = $.datepicker.formatDate('D, d M yy', date, inst.settings);
-					$('#dateoutput').html('Chosen date is <b>' + dateText1 + '</b>; chosen date + 7 days yields <b>' + dateText2 + '</b>');
-				}
+			$(document).ready(function () {
+				$('.datepicker').datepicker({
+					dateFormat: 'yy-mm-dd',
+					onSelect: function(dateText, inst) {
+						var date = $.datepicker.parseDate(inst.settings.dateFormat || $.datepicker._defaults.dateFormat, dateText, inst.settings);
+						var dateText1 = $.datepicker.formatDate('D, d M yy', date, inst.settings);
+						date.setDate(date.getDate() + 7);
+						var dateText2 = $.datepicker.formatDate('D, d M yy', date, inst.settings);
+						$('#dateoutput').html('Chosen date is <b>' + dateText1 + '</b>; chosen date + 7 days yields <b>' + dateText2 + '</b>');
+					}
+				});
 			});
-		});
 		})(jQuery);
-		"
+
+JS
 		);
 
 		// Set static array
 		static::$loaded[__METHOD__] = true;
 
-		return;
-	}
-
-	public static function barchartTest($containerId, $urlId, $horizontal = true)
-	{
-		// Only load once
-		if (isset(static::$loaded[__METHOD__]))
-		{
-			return;
-		}
-
-		$orientation = ($horizontal == true) ? 'horizontal' : 'vertical';
-
-		// Depends on jQuery UI
-		$document = JFactory::getDocument();
-		$document->addScript('components/com_trackerstats/media/js/jquery-1.9.1.min.js', 'text/javascript', false);
-		$document->addScript('components/com_trackerstats/media/js/jquery-noconflict.js', 'text/javascript', false);
-		$document->addScript('components/com_trackerstats/media/js/jquery.jqplot.min.js', 'text/javascript', false);
-		$document->addScript('components/com_trackerstats/media/js/jqplot.barRenderer.min.js', 'text/javascript', true);
-		$document->addScript('components/com_trackerstats/media/js/jqplot.categoryAxisRenderer.min.js', 'text/javascript', true);
-		$document->addScript('components/com_trackerstats/media/js/jqplot.pointLabels.min.js', 'text/javascript', true);
-		$document->addScript('components/com_trackerstats/media/js/barchartTest.js', 'text/javascript', true);
-		$document->addStyleSheet(JUri::root(true) . '/components/com_trackerstats/media/css/jquery.jqplot.min.css');
-
-		// Attach sortable to document
-		$document->addScriptDeclaration("
-				(function ($){
-				$(document).ready(function (){
-				var barchart = new $.JQPLOTBarchartTest('" . $containerId . "','" . $urlId . "','" . $orientation . "','" . $stackSeries . "','" . $barMargin . "');
-	});
-	})(jQuery);
-				"
-		);
-		$document->addScriptDeclaration("
-				(function ($){
-				$(document).ready(function (){
-				$('button.dataUpdate').click(function() {
-				$('#" . $containerId . "').empty();
-				// add the form variables to the URL
-				var period = $('#period').val();
-				var type = $('#type').val();
-				var href = $('#" . $urlId . "').attr('data-href');
-				href = href + '&period=' + period + '&activity_type=' + type;
-				$('#" . $urlId . "').attr('data-href', href);
-				var barChart = new $.JQPLOTBarchartTest('" . $containerId . "','" . $urlId . "','" . $orientation . "','" . $stackSeries . "','" . $barMargin . "');
-	});
-	});
-	})(jQuery);
-				"
-		);
-
-		// Set static array
-		static::$loaded[__METHOD__] = true;
 		return;
 	}
 }
